b467ac66bfe1d6ce1379c768d9d0292835d6199b
rabobahago <rabobahago@gmail.com>
2023-01-03 23:05:58 +0100

    test fund function with different variables added

--- test/unit/fundMe.test.js
+++ test/unit/fundMe.test.js
@@ -1,9 +1,10 @@
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 const { deployments, ethers, getNamedAccounts } = require("hardhat")
 describe("FundMe", async () => {
     let fundMe
     let deployer
     let MockV3Aggregator
+    let sendValue = ethers.utils.parseEther("1")
     beforeEach(async function () {
         deployer = (await getNamedAccounts()).deployer
         await deployments.fixture(["all"])
@@ -18,6 +19,27 @@ describe("FundMe", async () => {
         it("It set the aggregator addresses correctly", async function () {
             const response = await fundMe.getPriceFeed()
             assert.equal(response, MockV3Aggregator.address)
+        })
+    })
+    describe("fund", function () {
+        // https://ethereum-waffle.readthedocs.io/en/latest/matchers.html
+        // could also do assert.fail
+        it("Fails if you don't send enough ETH", async () => {
+            await expect(fundMe.fund()).to.be.revertedWith(
+                "You need to spend more ETH!"
+            )
+        })
+        // we could be even more precise here by making sure exactly $50 works
+        // but this is good enough for now
+        it("Updates the amount funded data structure", async () => {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.getAddressToAmountFunded(deployer)
+            assert.equal(response.toString(), sendValue.toString())
+        })
+        it("Adds funder to array of funders", async () => {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.getFunder(0)
+            assert.equal(response, deployer)
         })
     })
 })
